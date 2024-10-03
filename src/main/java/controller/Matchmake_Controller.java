44f9776ca3a17fc468936bed2530dfddc57ca1e3
Manu M. <manuel.mhernandez@alumnos.upm.es>
2024-10-03 23:37:36 +0200

    added matchmake and its controller

---
+++ src/main/java/controller/Matchmake_Controller.java
@@ -0,0 +1,29 @@
+package controller;
+import java.util.HashMap;
+import model.Matchmake;
+import model.Player;
+
+public class Matchmake_Controller
+{
+    private HashMap<Player,Player> matchmake;
+
+    public Matchmake_Controller()
+    {
+        
+    }
+
+    public void random_matchmake()
+    {
+
+    }
+
+    public void matchmake(Player playerA, Player playerB)
+    {
+       //if this playerA has a matchmake with PlayerB, then don´t do matchmake
+    }
+
+    public void erase_matchmaking(Matchmake matchmake)
+    {
+        
+    }
+}
---
+++ src/main/java/model/Matchmake.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Matchmake
+{
+    private Player playerA;
+    private Player playerB;
+
+    public Matchmake(Player playerA,Player playerB)
+    {
+        this.playerA = playerA;
+        this.playerB = playerB;
+    }
+
+    public Player getPlayerA() {
+        return playerA;
+    }
+
+    public void setPlayerA(Player playerA) {
+        this.playerA = playerA;
+    }
+
+    public Player getPlayerB() {
+        return playerB;
+    }
+
+    public void setPlayerB(Player playerB) {
+        this.playerB = playerB;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.playerA.getName() + " vs " + this.playerB.getName();
+    }
+    
+}
