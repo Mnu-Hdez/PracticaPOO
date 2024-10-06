22cd02d51e58c2fc197f00f2c3ba67de91e86394
Manuel Moreno Hernandez <65139967+Mnu-Hdez@users.noreply.github.com>
2024-10-06 13:14:55 +0200

    Update Matchmake_View.java

--- src/main/java/view/Matchmake_View.java
+++ src/main/java/view/Matchmake_View.java
@@ -1,6 +1,14 @@
 package view;
 
+import model.Matchmake;
+
 public class Matchmake_View
 {
-    
+    public Matchmake_view(){}
+
+    public void show(Matchmake matchmake)
+    {
+        matchmake.toString();
+    }
 }
+
